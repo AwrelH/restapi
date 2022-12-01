ade4966c6428eb44a51a83536a2ecb6850753ad1
AwrelH <hiwa.awrel@gmail.com>
2022-12-01 14:40:23 +0000

    models.py correction

--- profiles/models.py
+++ profiles/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        profile.objects.create(owner=instance)
+        Profile.objects.create(owner=instance)
 
 
 post_save.connect(create_profile, sender=User)
