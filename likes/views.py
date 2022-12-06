1a308face7957e5080172ceaf8dc9282401c09bb
AwrelH <hiwa.awrel@gmail.com>
2022-12-06 12:21:31 +0000

    added likes views and crud functionality

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Likes
+from .serializers import LikesSerializer
 
-# Create your views here.
+
+class LikesList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikesSerializer
+    queryset = Likes.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikesDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LikesSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Likes.objects.all()
