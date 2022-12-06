4f3a914d72f41452078426036d2fd9d7f5cbee1d
AwrelH <hiwa.awrel@gmail.com>
2022-12-06 08:10:53 +0000

    add image filter and validation in serializer

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,16 @@ class Post(models.Model):
     """
     post model
     """
+    image_filter_choices = [
+        ('_1977', '1977'), ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+        ('nashville', 'Nashville'), ('rise', 'Rise'),
+        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+        ('walden', 'Walden'), ('xpro2', 'X-pro II')
+        ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
@@ -14,8 +24,11 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_ocvliz.jpg', blank=True
     )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal')
+
     class Meta:
         ordering = ['-created_on']
-        
+
     def __str__(self):
-        return f'{self.id} {self.title}'
+        return f'{self.id} {self.title} '
